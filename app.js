38e6c94f3351e70938b73ff0c2424f899646a527
JisanFahim <fahimjason@users.noreply.github.com>
2022-03-13 01:56:50 +0600

    Done socket.io setting in server.

--- app.js
+++ app.js
@@ -61,7 +61,13 @@ app.use((error, req, res, next) => {
 
 mongoose.connect(MONGODB_URI)
     .then(() => {
-        app.listen(8080);
         console.log('Connected to server');
+
+        const server = app.listen(8080);
+        const io = require('socket.io')(server);
+
+        io.on('connection', socket => {
+            console.log('Client connected');
+        });
     })
     .catch(err => console.log(err));
