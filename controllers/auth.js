8d2f95fda27e3467648ea80ee3454b88718af492
JisanFahim <fahimjason@users.noreply.github.com>
2022-03-23 21:45:27 +0600

    Tested auth-controller.

--- controllers/auth.js
+++ controllers/auth.js
@@ -64,14 +64,16 @@ exports.login = async (req, res, next) => {
             {
                 email: loadedUser.email,
                 userId: loadedUser._id.toString()
-            }, 'somesupersecretsecret',
+            }, 'supersecretsecret',
             { expiresIn: '1h' }
         );
 
         res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+        return;
     } catch (err) {
         if (!err.statusCode) err.statusCode = 500;
         next(err);
+        return err;
     }
 };
 
